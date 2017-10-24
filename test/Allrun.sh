a3613eb36b1ae60a5cdceba0dce2effa504a369d
Hermes Droghetti <hermes.droghetti@gmail.com>
2017-10-24 12:03:17 +0200

    mod 1

--- test/Allrun.sh
+++ test/Allrun.sh
@@ -5,5 +5,5 @@ export LD_LIBRARY_PATH=/home/hermes/Desktop/Programmi/Lammps/lammps-14May16/src:
 export LD_LIBRARY_PATH=/home/hermes/Desktop/Programmi/MPI/install/bin:$LD_LIBRARY_PATH
 $HOME/Desktop/Programmi/MPI/install/bin/mpirun -n 2 AnonNewtonianIcoFoam -parallel >& log
 reconstructPar
-
+rm -r processor*
 
