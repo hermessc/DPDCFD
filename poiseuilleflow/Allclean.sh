568b4e892d1b0f0b8f2c08034d6c2f48c0007aa4
Hermes Droghetti <hermes.droghetti@gmail.com>
2017-10-26 10:56:43 +0200

    cambiata storageMatrix e ciclco di run, adesso se il flag vale 1, le simulazioni di lammps non vengono eseguite

--- poiseuilleflow/Allclean.sh
+++ poiseuilleflow/Allclean.sh
@@ -8,4 +8,12 @@ rm -r 5*
 rm -r 4*
 rm -r 6*
 rm -r processor*
+cd 0/
+mv U.bkp U; cp U U.bkp
+mv p.bkp p; cp p p.bkp
+mv shearRate.bkp sharRate; cp shearRate shearRate.bkp
+mv viscoDPD.bkp viscoDPD; cp viscoDPD viscoDPD.bkp
+cd ..
+
+
 
